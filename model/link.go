a860d3c5d825816520439cdd4405c4b73bcd3e6b
Krunoslav Husak <kruno@binel.hr>
2018-02-25 11:21:22 +0100

    Added function to convert name to numerical id and vice versa.

--- model/link.go
+++ model/link.go
@@ -1,6 +1,31 @@
 package model
 
+import (
+	"strings"
+)
+
+const validChars = "ABCDEFHJKLMNPRSTUVXYZabcdefgijkmnprstuvxyz23456789"
+
 // Link represent one shortened link
 type Link struct {
 	URL string `json:"url"`
+}
+
+// GetLinkNameFromID gets name from numerical ID
+func GetLinkNameFromID(id int) string {
+	name := ""
+	for id > 0 {
+		name = string(validChars[id%50]) + name
+		id = id / 50
+	}
+	return name
+}
+
+// GetLinkIDFromName gets ID from name
+func GetLinkIDFromName(name string) int {
+	id := 0
+	for i := 0; i < len(name); i++ {
+		id = 50*id + (strings.Index(validChars, string(name[i])))
+	}
+	return id
 }
