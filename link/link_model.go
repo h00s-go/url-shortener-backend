13ca80eea741819b9774b1a559901f7ccb12b78c
Krunoslav Husak <kruno@binel.hr>
2018-03-18 00:05:39 +0100

    Moved link constants to separate file

--- link/link_model.go
+++ link/link_model.go
@@ -6,38 +6,6 @@ import (
 	"strings"
 )
 
-// Do not change this after the links are inserted in database
-// It will break getting and inserting new links
-const validChars = "ABCDEFHJKLMNPRSTUVXYZabcdefgijkmnprstuvxyz23456789"
-
-const sqlInsertLink = `
-INSERT INTO links (
-	name, url, view_count, client_address, created_at
-)
-VALUES (
-	$1, $2, $3, $4, $5
-)
-RETURNING id
-`
-const sqlUpdateLinkName = `
-UPDATE links SET name = $1 WHERE id = $2
-`
-const sqlGetLinkByID = `
-SELECT id, name, url, view_count, client_address, created_at
-FROM links
-WHERE id = $1
-`
-const sqlGetLinkByName = `
-SELECT id, name, url, view_count, client_address, created_at
-FROM links
-WHERE name = $1
-`
-const sqlGetLinkByURL = `
-SELECT id, name, url, view_count, client_address, created_at
-FROM links
-WHERE url = $1
-`
-
 // Link represent one shortened link
 type Link struct {
 	ID            int    `json:"id"`
