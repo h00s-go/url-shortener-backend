79719c4e4eaf20de39fff2247e3a15cfd596c8b6
Krunoslav Husak <kruno@binel.hr>
2018-03-18 00:49:56 +0100

    Support for posting link
    Feels dirty, needs to be reorganized/rewrited.

--- link/link_controller.go
+++ link/link_controller.go
@@ -3,11 +3,17 @@ package link
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/h00s/url-shortener-backend/db"
+	"github.com/h00s/url-shortener-backend/host"
 )
 
 // Controller for Link methods
 type Controller struct {
 	db *db.Database
+}
+
+// PostLinkData represents data which is sent when posting new url
+type PostLinkData struct {
+	URL string `json:"url" binding:"required"`
 }
 
 // NewController creates new link controller
@@ -30,8 +36,25 @@ func (lc *Controller) GetLink(c *gin.Context) {
 }
 
 // PostLink adds new link
-func (*Controller) PostLink(c *gin.Context) {
-	c.JSON(200, gin.H{
-		"result": "ok",
-	})
+func (lc *Controller) PostLink(c *gin.Context) {
+	var postLinkData PostLinkData
+	if err := c.BindJSON(&postLinkData); err == nil {
+		err = host.IsValid(postLinkData.URL)
+		if err != nil {
+			c.JSON(404, err)
+			return
+		}
+		l, err := InsertLink(lc, postLinkData.URL, "127.0.0.1")
+		if err == nil {
+			c.JSON(200, l)
+		} else {
+			c.JSON(404, gin.H{
+				"message": "error while inserting link",
+			})
+		}
+	} else {
+		c.JSON(404, gin.H{
+			"message": "request is invalid",
+		})
+	}
 }
