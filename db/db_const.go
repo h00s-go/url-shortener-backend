0b7ff0b63c0a5d63c91908549eaad8e9f29871f9
Krunoslav Husak <kruno@binel.hr>
2018-03-26 12:05:29 +0200

    Removed view_count from DB links table
    Link views will be done in separate table with additional information.
    Also removed NOT NULL constraint from name column.

--- db/db_const.go
+++ db/db_const.go
@@ -17,9 +17,8 @@ INSERT INTO schema (version) VALUES (1)
 const sqlCreateLinks = `
 CREATE TABLE IF NOT EXISTS links (
 	id serial PRIMARY KEY,
-	name text UNIQUE NOT NULL,
+	name text UNIQUE,
 	url text UNIQUE NOT NULL,
-	view_count integer DEFAULT 0,
 	client_address inet NOT NULL,
 	created_at timestamp NOT NULL
 )
